b6ffe5d6368c6e8aa2b5cb5fb513073e07cfbba4
davidm00 <davidnmosesjr@gmail.com>
2022-03-05 04:00:14 -0500

    Change log

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import React from "react";
 import * as Env from "./environment";
 import Parse from "parse";
 import RoutesView from "./Components/RoutesView";
-// import { ThemeProvider as MuiThemeProvider } from "@mui/styles";
-// import { ThemeProvider } from "@emotion/react";
-// import CssBaseline from "@mui/material/CssBaseline";
-
-// import { primaryTheme } from "./Themes/primaryTheme";
 import EmotionTheme from "./Common/EmotionTheme";
 
 Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
@@ -16,14 +11,6 @@ Parse.serverURL = Env.SERVER_URL;
 
 function App() {
   return (
-    // <MuiThemeProvider theme={primaryTheme}>
-    //   <ThemeProvider theme={primaryTheme}>
-    //     {/* <CssBaseline /> */}
-    //     <div className="App">
-    //       <RoutesView />
-    //     </div>
-    //   </ThemeProvider>
-    // </MuiThemeProvider>
     <EmotionTheme>
       <div className="App">
         <RoutesView />
