5691ecbbd4f66e3625fae265489298f0245af611
davidm00 <davidnmosesjr@gmail.com>
2022-04-01 00:44:09 -0400

    env

---
+++ src/environment.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: 'E1khzoQWSwxLxRA2oZYxz9Z6RFwDLWtjHag6bK77',
+    JAVASCRIPT_KEY: 'k781xWMdSc0TVWP1eMGKGBP5Yg614Qem3AmbD3YT',
+    SERVER_URL: 'https://parseapi.back4app.com'
+}
